9d918bc5df57bc241bcd14f7e24ea37e21663f14
VadimYanuschik <vadim_wower@list.ru>
2021-08-29 14:47:29 +0300

    add input number validation

--- src/components/calculator/calculator.component.js
+++ src/components/calculator/calculator.component.js
@@ -8,14 +8,21 @@ import Select from '../select/select.component';
 import {startCalculations} from '../../redux/deposit/deposit.actions';
 
 function Calculator({startCalculations}) {
-    const [depositAmount, setDepositAmount] = useState(150);
-    const [currency, setCurrency] = useState('BYN');
-    const [monthCount, setMonthCount] = useState(18);
-    const [interestRate, setInterestRate] = useState(18);
-    const [withCapitalization, setWithCapitalization] = useState(true);
+    const [depositAmount, setDepositAmount] = useState('');
+    const [currency, setCurrency] = useState('');
+    const [monthCount, setMonthCount] = useState('');
+    const [interestRate, setInterestRate] = useState('');
+    const [withCapitalization, setWithCapitalization] = useState(false);
 
     const handleClick = () => {
         startCalculations({depositAmount, currency, monthCount, interestRate, withCapitalization})
+    }
+
+    const handleLetters = (e) => {
+        e = e.replace(/[^0-9\.]/g,'');
+        if(e.split('.').length>2)
+            e = e.replace(/\.+$/,"");
+        return e
     }
 
     return (
@@ -25,10 +32,10 @@ function Calculator({startCalculations}) {
                 <TitleText>Bank</TitleText>
             </TitleContainer>
             <FormContainer>
-                <Input onChange={e => {setDepositAmount(e.target.value)}} value={depositAmount} placeholder={'Сумма вклада'}/>
+                <Input onChange={e => {setDepositAmount(handleLetters(e.target.value))}} type='number' value={depositAmount} placeholder={'Сумма вклада'}/>
                 <Select onChange={e => {setCurrency(e.target.value)}} placeholder={'Выберите валюту'}/>
-                <Input onChange={e => setMonthCount(e.target.value)} value={monthCount} placeholder={'Кол-во месяцев'}/>
-                <Input onChange={e => setInterestRate(e.target.value)} value={interestRate} placeholder={'Годовая ставка, %'}/>
+                <Input onChange={e => setMonthCount(handleLetters(e.target.value))} type='number' value={monthCount} placeholder={'Кол-во месяцев'}/>
+                <Input onChange={e => setInterestRate(handleLetters(e.target.value))} type='number' value={interestRate} placeholder={'Годовая ставка, %'}/>
                 <Checkbox onChange={e => setWithCapitalization(!withCapitalization)} checked={withCapitalization} id={'form-button'} labelText={'С капитализацией'}/>
                 <Button onClick={handleClick} text={'Рассчитать'}/>
             </FormContainer>
